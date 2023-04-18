3e1e1cf1a39cc3c37889062bc9d1959d926de6ef
elias <politico@mail.ru>
2023-04-18 16:42:15 +0300

    bootstrap vue & axios added

---
+++ src/components/ProfileSettingsComponent.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <div>This is profile settings component</div>
+    <b-container>
+      <b-row>
+        <b-col>1 of 3</b-col>
+        <b-col>2 of 3</b-col>
+        <b-col>3 of 3</b-col>
+      </b-row>
+    </b-container>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ProfileSettingsComponent",
+};
+</script>
+
+<style scoped></style>
---
+++ src/views/ProfileView.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <div>This is profile view</div>
+    <ProfileSettingsComponent />
+  </div>
+</template>
+
+<script>
+import ProfileSettingsComponent from "@/components/ProfileSettingsComponent.vue";
+
+export default {
+  name: "ProfileView",
+  components: {
+    ProfileSettingsComponent,
+  },
+};
+</script>
